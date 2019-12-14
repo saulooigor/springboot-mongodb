25e3125a40b7faaf2fbe06d02f38788e1232b5ad
saulooigor <saulo.igor.alves@gmail.com>
2019-12-13 23:19:46 -0300

    Create exception

--- src/main/java/com/iotsaulo/springbootmongodb/services/UserService.java
+++ src/main/java/com/iotsaulo/springbootmongodb/services/UserService.java
@@ -1,12 +1,14 @@
 package com.iotsaulo.springbootmongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.iotsaulo.springbootmongodb.domain.User;
 import com.iotsaulo.springbootmongodb.repository.UserRepository;
+import com.iotsaulo.springbootmongodb.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,11 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
 }
