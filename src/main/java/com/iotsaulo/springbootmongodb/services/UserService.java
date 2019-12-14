1f1dfb4a28e9c5340504cfb6efe9f4dc194982e9
saulooigor <saulo.igor.alves@gmail.com>
2019-12-14 00:19:02 -0300

    Update created

--- src/main/java/com/iotsaulo/springbootmongodb/services/UserService.java
+++ src/main/java/com/iotsaulo/springbootmongodb/services/UserService.java
@@ -35,6 +35,17 @@ public class UserService {
 		repository.deleteById(id);
 	}
 	
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repository.save(newObj);
+	}
+	
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
+
 	public User fromDTO (UserDTO dto) {
 		User user = new User(dto.getId(), dto.getName(), dto.getEmail());
 		return user;
