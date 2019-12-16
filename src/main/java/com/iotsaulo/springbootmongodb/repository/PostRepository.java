3a7dc5b3f6903ad15fef298557e03f0bcf99236b
saulooigor <saulo.igor.alves@gmail.com>
2019-12-16 15:15:11 -0300

    Using Query regex

--- src/main/java/com/iotsaulo/springbootmongodb/repository/PostRepository.java
+++ src/main/java/com/iotsaulo/springbootmongodb/repository/PostRepository.java
@@ -3,11 +3,15 @@ package com.iotsaulo.springbootmongodb.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.iotsaulo.springbootmongodb.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String> {
-	
-	List<Post> findByTitleContainingIgnoreCase (String text);
+
+	@Query("{'title': { $regex: ?0, $options: 'i' }}")
+	List<Post> findByTitle(String text);
+
+	List<Post> findByTitleContainingIgnoreCase(String text);
 
 }
