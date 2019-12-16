f09a83939c3ebe89f5f75edb0e0b30a8d949e4dc
saulooigor <saulo.igor.alves@gmail.com>
2019-12-16 00:01:18 -0300

    Implement post findById

---
+++ src/main/java/com/iotsaulo/springbootmongodb/services/PostService.java
@@ -0,0 +1,23 @@
+package com.iotsaulo.springbootmongodb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.iotsaulo.springbootmongodb.domain.Post;
+import com.iotsaulo.springbootmongodb.repository.PostRepository;
+import com.iotsaulo.springbootmongodb.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repository;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+}
