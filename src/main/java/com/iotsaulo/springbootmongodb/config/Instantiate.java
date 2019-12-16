23942fa82777303006dd752e72edf05912c71585
saulooigor <saulo.igor.alves@gmail.com>
2019-12-15 22:53:43 -0300

    Refactor Post and create PostDTO

--- src/main/java/com/iotsaulo/springbootmongodb/config/Instantiate.java
+++ src/main/java/com/iotsaulo/springbootmongodb/config/Instantiate.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.iotsaulo.springbootmongodb.domain.Post;
 import com.iotsaulo.springbootmongodb.domain.User;
+import com.iotsaulo.springbootmongodb.dto.AuthorDTO;
 import com.iotsaulo.springbootmongodb.repository.PostRepository;
 import com.iotsaulo.springbootmongodb.repository.UserRepository;
 
@@ -36,8 +37,8 @@ public class Instantiate implements CommandLineRunner {
 		
 		repoUser.saveAll(Arrays.asList(maria, alex, bob));
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo! Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo! Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 		
 		repoPost.saveAll(Arrays.asList(post1, post2));
 	}
