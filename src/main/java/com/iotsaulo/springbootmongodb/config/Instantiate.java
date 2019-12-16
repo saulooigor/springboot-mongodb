b9b63bfa0cd7e9597b909b8bc24aece4efad2ef1
saulooigor <saulo.igor.alves@gmail.com>
2019-12-16 00:24:08 -0300

    Add comments in Posts

--- src/main/java/com/iotsaulo/springbootmongodb/config/Instantiate.java
+++ src/main/java/com/iotsaulo/springbootmongodb/config/Instantiate.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.iotsaulo.springbootmongodb.domain.Post;
 import com.iotsaulo.springbootmongodb.domain.User;
 import com.iotsaulo.springbootmongodb.dto.AuthorDTO;
+import com.iotsaulo.springbootmongodb.dto.CommentDTO;
 import com.iotsaulo.springbootmongodb.repository.PostRepository;
 import com.iotsaulo.springbootmongodb.repository.UserRepository;
 
@@ -40,11 +41,20 @@ public class Instantiate implements CommandLineRunner {
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo! Abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 		
+		CommentDTO cm1 = new CommentDTO("Boa viagem mano", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO cm2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO cm3 = new CommentDTO("Tenha um ótimo dia", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(cm1, cm2));
+		post2.getComments().add(cm3);
+		
 		repoPost.saveAll(Arrays.asList(post1, post2));
 		
 		maria.getPosts().add(post1);
 		maria.getPosts().add(post2);
 		repoUser.save(maria);
+		
+		
 	}
 
 }
